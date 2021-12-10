b3673590a9426a26699a33fab5e95a314b65bc65
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-10 11:33:46 -0500

    managed to grab the weather data

--- app/apis/weather.py
+++ app/apis/weather.py
@@ -1,10 +1,22 @@
-import urllib.request as request
+import urllib.parse as urlparse, urllib.request as urlrequest
+from urllib.parse import urlencode
 import json, socket
 
 apiKey = "a591cc0b3ce141668cc142051211012"
 
+ip = urlrequest.urlopen('https://ident.me').read().decode('utf8')
+
 def getWeatherForCurrentLocation():
     url = "http://api.weatherapi.com/v1/current.json"
-    siteData = request.urlopen(url)
-    params = {'key': apiKey, 'q': socket.gethostname()}
+    params = {'key': apiKey, 'q': ip}
+
+    urlParts = list(urlparse.urlparse(url))
+    query = dict(urlparse.parse_qsl(urlParts[4]))
+    query.update(params)
     
+    urlParts[4] = urlparse.urlencode(query)
+    siteData = urlrequest.urlopen(urlparse.urlunparse(urlParts))
+    data = json.load(siteData)
+    print(data)
+
+getWeatherForCurrentLocation()
