5cf6269a9f7fa4f0b1c8a892ac0b5de7a00d2cfd
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-14 14:33:26 -0500

    kanye part of api done

--- app/apis/kanyeQuote.py
+++ app/apis/kanyeQuote.py
@@ -1,11 +1,12 @@
 import urllib.request as urlrequest
+import urllib
 import json
 
 def getQuote():
-    url = 'api.kanye.rest'
+    url = 'https://api.kanye.rest'
 
-    siteData = urlrequest.urlopen(url)
+    headers={'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
+    request = urlrequest.Request(url, None, headers)
+    siteData = urlrequest.urlopen(request)
 
-    print(json.load(siteData))
-
-getQuote()
+    return json.load(siteData)['quote']
