291b868193e1537862123e40ade63ff9f5fa84f9
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-15 20:19:06 -0500

    added notes on how the api returns things

--- app/apis/weather.py
+++ app/apis/weather.py
@@ -17,3 +17,7 @@ def getWeatherForCurrentLocation():
     siteData = urlrequest.urlopen(urlparse.urlunparse(urlParts))
     data = json.load(siteData)
     return data
+
+# example formatting:
+
+# {'location': {'name': 'Washington Heights', 'region': 'New York', 'country': 'United States of America', 'lat': 40.85, 'lon': -73.94, 'tz_id': 'America/New_York', 'localtime_epoch': 1639617025, 'localtime': '2021-12-15 20:10'}, 'current': {'last_updated_epoch': 1639612800, 'last_updated': '2021-12-15 19:00', 'temp_c': 11.1, 'temp_f': 52.0, 'is_day': 0, 'condition': {'text': 'Overcast', 'icon': '//cdn.weatherapi.com/weather/64x64/night/122.png', 'code': 1009}, 'wind_mph': 0.0, 'wind_kph': 0.0, 'wind_degree': 164, 'wind_dir': 'SSE', 'pressure_mb': 1031.0, 'pressure_in': 30.43, 'precip_mm': 0.0, 'precip_in': 0.0, 'humidity': 71, 'cloud': 100, 'feelslike_c': 9.4, 'feelslike_f': 48.9, 'vis_km': 16.0, 'vis_miles': 9.0, 'uv': 1.0, 'gust_mph': 13.6, 'gust_kph': 22.0}}
