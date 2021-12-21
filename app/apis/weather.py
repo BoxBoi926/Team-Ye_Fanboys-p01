18543d587b2f9a233396fc0be9cdf750d747ff40
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-21 09:11:24 -0500

    added try catch to anything that talks to the internet, splitting the get ip address api up into its own python script

--- app/apis/weather.py
+++ app/apis/weather.py
@@ -1,22 +1,28 @@
 import urllib.parse as urlparse, urllib.request as urlrequest
 import json
+import getIpAddress
 
 apiKey = "a591cc0b3ce141668cc142051211012"
 
-ip = urlrequest.urlopen('https://ident.me').read().decode('utf8')
+ip = getIpAddress.getIp()
 
 def getWeatherForCurrentLocation():
-    url = "http://api.weatherapi.com/v1/current.json"
-    params = {'key': apiKey, 'q': ip}
+    try:
+        url = "http://api.weatherapi.com/v1/current.json"
+        params = {'key': apiKey, 'q': ip}
 
-    urlParts = list(urlparse.urlparse(url))
-    query = dict(urlparse.parse_qsl(urlParts[4]))
-    query.update(params)
-    
-    urlParts[4] = urlparse.urlencode(query)
-    siteData = urlrequest.urlopen(urlparse.urlunparse(urlParts))
-    data = json.load(siteData)
-    return data
+        urlParts = list(urlparse.urlparse(url))
+        query = dict(urlparse.parse_qsl(urlParts[4]))
+        query.update(params)
+        
+        urlParts[4] = urlparse.urlencode(query)
+        siteData = urlrequest.urlopen(urlparse.urlunparse(urlParts))
+        data = json.load(siteData)
+        return data
+    except Exception as issue:
+        print('Unable to get weather location')
+        print(issue)
+        return {}
 
 # example formatting:
 
