776667bf722347ae266001a09412c1f33d0540d6
DavidASC20 <djchong24@gmail.com>
2021-12-22 09:05:29 -0500

    ;-;

--- app/apis/weather.py
+++ app/apis/weather.py
@@ -51,7 +51,6 @@ def getForecastForCurrentLocation():
     except Exception as issue:
         print('Unable to get weather location')
         print(issue)
-<<<<<<< HEAD
         return {}
 
 # example formatting:
@@ -61,6 +60,3 @@ def getForecastForCurrentLocation():
 # example formatting:
 
 # {'location': {'name': 'Washington Heights', 'region': 'New York', 'country': 'United States of America', 'lat': 40.85, 'lon': -73.94, 'tz_id': 'America/New_York', 'localtime_epoch': 1639617025, 'localtime': '2021-12-15 20:10'}, 'current': {'last_updated_epoch': 1639612800, 'last_updated': '2021-12-15 19:00', 'temp_c': 11.1, 'temp_f': 52.0, 'is_day': 0, 'condition': {'text': 'Overcast', 'icon': '//cdn.weatherapi.com/weather/64x64/night/122.png', 'code': 1009}, 'wind_mph': 0.0, 'wind_kph': 0.0, 'wind_degree': 164, 'wind_dir': 'SSE', 'pressure_mb': 1031.0, 'pressure_in': 30.43, 'precip_mm': 0.0, 'precip_in': 0.0, 'humidity': 71, 'cloud': 100, 'feelslike_c': 9.4, 'feelslike_f': 48.9, 'vis_km': 16.0, 'vis_miles': 9.0, 'uv': 1.0, 'gust_mph': 13.6, 'gust_kph': 22.0}}
-=======
-        return {}
->>>>>>> b40694a19a4732d0d59da168852b7db50e48c1e5
