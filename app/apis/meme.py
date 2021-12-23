584a8259cb91e33fd232c4f037610521a107a1f4
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-12-23 12:18:39 -0500

    removing nsfw memes now on backend

--- app/apis/meme.py
+++ app/apis/meme.py
@@ -6,7 +6,10 @@ def getMeme(returnNum: int = 10, resource: str = "kanye"):
         url = "https://meme-api.herokuapp.com/gimme/"+resource+"/"+str(returnNum)
         siteData = urlrequest.urlopen(url)
 
-        return json.load(siteData)['memes']
+        results = json.load(siteData)['memes']
+        results = [result for result in results if result['nsfw']]
+
+        return results
     except Exception as issue:
         print("Error encountered while loading memes.")
         print(issue)
