fe40e63b9d0d3e58cd05c9394fb2115d7eb42301
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-01-04 16:03:07 -0500

    fix issue where minute could end up being single digit

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -7,7 +7,7 @@ setInterval(() => {
 
     const hours = date.getHours();
     const mins = date.getMinutes();
-    dateElement.innerHTML = `${hours < 10 ? "0" + hours : hours}:${mins}`;
+    dateElement.innerHTML = `${hours < 10 ? "0" + hours : hours}:${mins < 10 ? "0"+mins : mins}`;
     if (12 > hours && hours >= 5) {
         greeting.innerHTML = "Good morning, "+userName.innerHTML+"!";
     } else if (17 > hours && hours >= 12) {
