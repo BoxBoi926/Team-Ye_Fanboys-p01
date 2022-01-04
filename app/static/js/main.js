09c080417124751fbf806b5f910eb3f25618a685
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2022-01-04 15:56:48 -0500

    fix issue where the greeting was statically timed

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,4 +1,6 @@
 const dateElement = document.getElementById("date");
+const greeting = document.getElementById("greeting");
+const userName = document.getElementById("name");
 
 setInterval(() => {
     const date = new Date();
@@ -6,4 +8,13 @@ setInterval(() => {
     const hours = date.getHours();
     const mins = date.getMinutes();
     dateElement.innerHTML = `${hours < 10 ? "0" + hours : hours}:${mins}`;
+    if (12 > hours && hours >= 5) {
+        greeting.innerHTML = "Good morning, "+userName.innerHTML+"!";
+    } else if (17 > hours && hours >= 12) {
+        greeting.innerHTML = "Good afternoon, "+userName.innerHTML+"!";
+    } else if (21 > hours && hours >= 17) {
+        greeting.innerHTML = "Good evening, "+userName.innerHTML+"!";
+    } else {
+        greeting.innerHTML = "Good night, " + userName.innerHTML + "!";
+    }
 }, 1000);
